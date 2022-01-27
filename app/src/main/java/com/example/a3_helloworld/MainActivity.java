793e0f7e92371b9d2a7161e6ac5c718ecf9b81e1
mortessa99 <smortezajahan@gmail.com>
2022-01-27 20:24:28 +0330

    end of toast lesson (26)

--- app/src/main/java/com/example/a3_helloworld/MainActivity.java
+++ app/src/main/java/com/example/a3_helloworld/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.a3_helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //all of the UI components are Views.(for 'buttonIsClicked' )
     //some times without 'public' app crashing like this app.(after click)
     //and without 'public' color method is gray Although its use in the activity_main.---(now is green)
+   @SuppressLint("WrongConstant")
    public void buttonIsClicked(View buttonIsView){
        TextView txtView = findViewById(R.id.txtView);
        EditText edtName = findViewById(R.id.edtName);
@@ -23,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
         Log.i("mytext",txtView.getText().toString());
         Log.i("myvalue",edtName.getText().toString());
         Log.i("myphone",edtPhone.getText().toString());
+       Toast.makeText(this,
+               "Your Name is"+" "+edtName.getText().toString()+
+                       "\n"+"Your Phone is"+" "+edtPhone.getText().toString(),
+               (1)).show();
     }
 
 
